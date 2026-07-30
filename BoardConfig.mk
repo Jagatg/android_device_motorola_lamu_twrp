16a14f037649d7c83789a1ec6f3b47190865171c
jagatg <jagathkrishnap73@gmail.com>
2026-07-30 08:08:24 +0530

    lamu: For building with minimal manifest

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,6 +5,11 @@
 
 DEVICE_PATH := device/motorola/lamu
 
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # A/B
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS := \
