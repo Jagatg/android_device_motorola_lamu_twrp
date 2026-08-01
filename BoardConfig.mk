e450c96cf7249df966ca807093eb9c120101912e
jagatg <jagathkrishnap73@gmail.com>
2026-08-01 09:54:06 +0000

    lamu: Changed a typo in kernel Cmdline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,7 +66,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 BOARD_MKBOOTIMG_INIT_ARGS += --header_version $(BOARD_INIT_BOOT_HEADER_VERSION)
 
-BOARD_VENDOR_CMDLINE := \
+BOARD_KERNEL_CMDLINE := \
     bootopt=64S3,32N2,64N2 \
     androidboot.selinux=permissive
 
